709591873379f98a5bb48b4f5686144f0d6324d8
Shilu <shilu842003@gmail.com>
2024-09-14 19:14:07 +0530

    update: load_test.py

--- load_test.py
+++ load_test.py
@@ -11,24 +11,25 @@ class QuickstartUser(HttpUser):
 
     @task(5)
     def io_task(self):
-        self.client.get("/io_task", name="/io_task")
+        self.client.get("io_task", name="/io_task")
 
     @task(5)
     def cpu_task(self):
-        self.client.get("/cpu_task", name="/cpu_task")
+        self.client.get("cpu_task", name="/cpu_task")
 
     @task(3)
     def random_sleep(self):
-        self.client.get("/random_sleep", name="/random_sleep")    
+        self.client.get("random_sleep", name="/random_sleep")    
 
     @task(10)
     def random_status(self):
-        self.client.get("/random_status", name="/random_status")
+        self.client.get("random_status", name="/random_status")
 
     @task(3)
     def chain(self):
-        self.client.get("/chain", name="/chain")
+        self.client.get("chain", name="/chain")
 
     @task()
     def random_sleep(self):
-        self.client.get("/error_test", name="/error_test") 
+        self.client.get("error_test", name="/error_test") 
+
